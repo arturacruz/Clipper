6e17be1aab37d1a67b28522c58d7f1b1a85a745e
Artur Cruz <arturacruz@icloud.com>
2024-10-01 23:02:01 -0300

    initial file chooser

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+static void on_open_response()
+{
+
+}
+
+static void onButtonClick(GtkButton *button, GtkWindow *window)
+{
+    const char *buttonLabel = gtk_button_get_label(button);
+
+    // If didn't click the button yet
+    if(g_strcmp0(buttonLabel, "Open") == 0)
+    {
+        // Set label to "Opening..."
+        gtk_button_set_label(button, "Opening...");
+
+        // Creates a new file dialog
+        GtkFileDialog *fileDialog = gtk_file_dialog_new();
+
+        // TODO: actually input some files
+        gtk_file_dialog_open(fileDialog, GTK_WINDOW(window), NULL, on_open_response, NULL);
+    }
+    else // If label is "Opening..."
+    {
+         gtk_button_set_label(button, "Open");
+    }  
+}
 
 static void activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *scrollWindow;
-    
+
+    GtkWidget *box;
+    GtkWidget *button;
+
     GtkWidget *textView;
     GtkTextBuffer *textBuffer;
     gchar *defaultText = "Ipsum louco latim doido";
@@ -17,9 +46,19 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_window_set_title(GTK_WINDOW(window), "Editor Foda");
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
-    // Creates a scrollable window and adds it as child of window
+    // Creates box and sets it as child of window
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
+    gtk_window_set_child(GTK_WINDOW(window), box);
+
+    // Creates button and adds it to box
+    button = gtk_button_new_with_label("Open");
+    g_signal_connect(button, "clicked", G_CALLBACK(onButtonClick), NULL);
+    gtk_box_append(GTK_BOX(box), button);
+
+    // Creates a scrollable window and adds it to box
     scrollWindow = gtk_scrolled_window_new();
-    gtk_window_set_child(GTK_WINDOW(window), scrollWindow);
+    gtk_box_append(GTK_BOX(box), scrollWindow);
 
     // Creates text view and buffer for text editing
     textView = gtk_text_view_new();
